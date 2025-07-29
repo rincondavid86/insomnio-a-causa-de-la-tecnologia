83ebc4d4e50baa095cd02c8d527453873f1cf5be
rincondavid86 <rincondavid863@gmail.com>
2025-07-28 22:18:31 -0500

    Add files via upload

---
+++ cursobasico/index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="El insomnio causado por la tecnología es un trastorno del sueño que se presenta cuando el uso prolongado de dispositivos electrónicos, especialmente antes de dormir, interfiere con la capacidad de conciliar o mantener el sueño. La exposición a la luz azul emitida por pantallas de celulares, tabletas, televisores y computadores altera la producción de melatonina, la hormona que regula el sueño. Esto provoca que el cuerpo no reconozca adecuadamente cuándo es hora de descansar.
+		Además, la estimulación constante del cerebro por notificaciones, redes sociales, videojuegos o series genera un estado de alerta que dificulta la relajación necesaria para dormir. El insomnio tecnológico es cada vez más común, especialmente entre adolescentes y adultos jóvenes, y puede derivar en problemas de concentración, irritabilidad, ansiedad y bajo rendimiento académico o laboral.">
+		<meta name="keywords" content="bebida, gaseosa, mejor gaseosa, Cola, liquido, bebida refrescante, productos Coca Cola,marcas de gaseosas, historia de Coca Cola">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>
+			Insomnio adolecente ¿A causa de la tecnologia?
+		</title>
+		<link rel="shortcut icon"  href="img/favicon-insomnio.png">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital@0;1&display=swap" rel="stylesheet">
+		<link rel="stylesheet"  href="css/normalize.css">
+		<link rel="stylesheet"  href="style.css">
+		<script src="https://kit.fontawesome.com/9828456893.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<a href="index.hyml">
+					<img src="img/logo-insomnio.jpg" class="logo">
+				</a>
+				<nav>
+					<a href="#inicio">Inicio</a>
+					<a href="#nosotros">Nosotros</a>
+					<a href="#servicios">Servicios</a>
+					<a href="#galeria">Galería</a>
+					<a href="#contactenos">Contactenos</a>
+				</nav>
+				<a href="#" class="hamb"><i class="fa-solid fa-bars"></i></a>
+			</div>
+		</header>
+		<main>
+			<section id="inicio">
+				<img src="img/insomnio.jpg">
+				<div class="bloque-inicio">
+					<h1>¿El insomnio es o no culpa de la tecnologia?</h1>
+					<p>
+						Vivimos en una era donde las pantallas nos acompañan día y noche… Algunas personas consideran que estas son las culpables de interrumpir el descanso, ¿realmente la tecnología será la culpable de que no podamos dormir bien?.
+						En esta página exploraremos las causas del insomnio, el impacto que tienen los dispositivos electrónicos en nuestro descanso, y si de verdad podemos culpar a la tecnología por nuestras noches de desvelo.
+						Prepárate para descubrir datos, estudios, consejos y reflexiones que te ayudarán a entender mejor este problema moderno.
+					</p>
+					<a href="#nosotros" class="boton boton-rojo">ver más</a>
+				</div>
+			</section>
+			<section id="nosotros" class="seccion">
+				<div class="container">
+					<p> En los últimos años, dormir bien se ha vuelto todo un reto para muchas personas, especialmente en un mundo donde el celular, las redes sociales, los videojuegos y las pantallas nos acompañan casi todo el día... y también la noche.
+					Aquí no buscamos juzgar ni señalar, sino entender mejor lo que está pasando. Exploraremos cómo nuestros hábitos digitales pueden afectar el sueño, qué señales debemos tener en cuenta y, sobre todo, qué podemos hacer para encontrar un equilibrio entre estar conectados y descansar de verdad.
+					Porque dormir no es perder el tiempo, es recargar la vida.
+					Y sí, aún en un mundo lleno de notificaciones, es posible tener noches tranquilas.</p>
+
+	
+				</div>
+			</section>
+			<section id="servicios" class="seccion">
+				<div class="container">
+					<div class="row">
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<video width="348.35" height="195.94" controls>
+  									<source src="img/insomnio.mp4" type="video/mp4">
+									</video>
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Tecnología y sueño: una relación perjudicial</h3>
+									<p>El insomnio relacionado con el uso excesivo de la tecnología es un fenómeno cada vez más común en la sociedad actual. La exposición prolongada a pantallas, especialmente antes de dormir, altera los ciclos naturales del sueño al interferir con la producción de melatonina, la hormona responsable de regular el descanso. Este problema afecta principalmente a adolescentes y adultos jóvenes, quienes suelen utilizar dispositivos electrónicos hasta altas horas de la noche. Comprender esta relación es esencial para promover hábitos digitales más saludables y mejorar la calidad del sueño.
+									</p>
+                                    <a href="#" class="boton boton-blanco">ver más</a>
+								</div>
+							</div>
+						</div>
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+								<img src="img/servicio.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>servicio 2</h3>
+									<p>La lata de Coca-Cola, con su contenido de 355 ml, ofrece la experiencia auténtica y refrescante que ha acompañado a generaciones. Su diseño icónico y su sabor inconfundible la convierten en la elección perfecta para disfrutar en cualquier momento del día. Elaborada con agua carbonatada, azúcar, colorante caramelo (E150d), ácido fosfórico, aromas naturales y cafeína, esta bebida proporciona una sensación de frescura y energía. Cada lata contiene aproximadamente 140 calorías y 39 gramos de azúcar, lo que la convierte en una opción ideal para acompañar tus comidas o para disfrutar sola. Además de su sabor, la lata de Coca-Cola destaca por su practicidad. Su tamaño compacto la hace fácil de transportar y almacenar, permitiéndote disfrutar de tu bebida favorita en cualquier lugar. Es importante recordar que, una vez abierta, la bebida debe consumirse en su totalidad para preservar su frescura y burbujeas.
+									</p>
+                                    <a href="#" class="boton boton-blanco">ver más</a>
+								</div>
+							</div>
+						</div>
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<video width="348.35" height="195.94" controls>
+  									<source src="img/tecnologia.mp4" type="video/mp4">
+									</video>
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Menos tecnología, más bienestar</h3>
+									<p>El video aborda el impacto del uso excesivo de dispositivos tecnológicos en la calidad del sueño. La constante interacción con celulares, computadores y otros aparatos electrónicos antes de dormir provoca una estimulación mental que dificulta el descanso. Además, la luz azul emitida por las pantallas altera el ritmo circadiano, retrasando el sueño y reduciendo su profundidad. Esta situación afecta especialmente a estudiantes y trabajadores jóvenes, quienes muchas veces no logran desconectarse adecuadamente durante la noche. Tomar conciencia sobre este problema permite adoptar medidas que favorezcan un descanso saludable y equilibrado.</p>
+                                    <a href="#" class="boton boton-blanco">ver más</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div style="text-align: center;">
+  					<a href="https://es.educaplay.com/recursos-educativos/24763014-preguntas_sobre_insomnio_y_tecnologia.html" target="_blank"
+   					 Class="boton boton-blanco">Haz clic aquí para jugar
+  					</a>
+				</div>
+			</section>
+			<section id="galeria">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/1.jpg">
+								<h4>Imagen1</h4>
+							</div>
+						</div>
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/22.jpg">
+								<h4>Imagen2</h4>
+							</div>
+						</div>
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/3.jpg">
+								<h4>Imagen3</h4>
+							</div>
+						</div>
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/4.jpg">
+								<h4>Imagen4</h4>
+							</div>
+						</div>
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/55.jpg">
+								<h4>Imagen5</h4>
+							</div>
+						</div>
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/66.jpg">
+								<h4>Imagen6</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="contactenos" class="seccion">
+				<iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=coca%20cola%20fusagasuga+(colombia)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+				<div class="container-fluid">
+					<div class="row">
+						<div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+							<form action="index.php" method="post">
+								<div class="form-block">
+									<input type="text" name="nombre" class="form-comtrol" placeholder="Nombre">
+								</div>
+								<div class="form-block">
+									<input type="email" name="email" class="form-comtrol" placeholder="Email">
+								</div>
+								<div class="form-block">
+									<textarea name="mensaje" placeholder="Mensaje"></textarea>
+								</div>
+								<div class="form-block bloque-ultimo">
+									<input type="submit" class="boton boton-negro" value="enviar">
+								</div>
+								<?php
+									if($_SERVER["REQUEST_METHOD"] == "POST")
+									{
+										$nombre = $_POST["nombre"];
+										$email = $_POST["email"];
+										$mensaje = $_POST["mensaje"];
+
+										if(isset($nombre))
+										{
+											if(isset($email))
+											{
+												if(isset($mensaje))
+												{
+													$para = "quirogadavid1002@gmail.com";
+													$asunto="Esto es una prueba";
+													$cuerpo = $nombre."\n".$email."\n".$mensaje;
+													$adicional = "From: noreplay@quirogadavid1002.com";
+
+													mail($para,$asunto,$cuerpo,$adicional);
+												?>
+													<p>Envio exitoso</p>
+												<?php 
+												}
+											}
+										}	
+									}
+								?>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		<footer class="seccion">
+			<div class="container">
+				<div class="row">
+					<div class="columna columna-25 columna-mobile-100">
+						<img src="img/logo-blanco.jpg" class="logo-footer">
+						<p>
+							En este espacio, valoramos cada opinión, experiencia o inquietud relacionada con el insomnio y el impacto de la tecnología en nuestro descanso.
+							Ya sea que tengas preguntas, quieras compartir tu historia, busques recomendaciones para mejorar tu sueño o simplemente necesites información.
+							Puedes contactarnos a través de:
+
+							📧 Correo electrónico
+
+							📱 Redes sociales
+
+							💬 Formulario de contacto
+
+						</p>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							temas relacionados
+						</h3>
+						<ul>
+							<li><a href="#">Insomnio</a></li>
+							<li><a href="#">Tecnologia</a></li>
+							<li><a href="#">Adolecentes</a></li>
+						</ul>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Datos de contacto
+						</h3>
+						<ul>
+							<li>carlos.quiroga.25@iem-ebenezer.com.co
+							Diaz.sharoon@iem-ebenezer.com.co</li>
+							<li>+57 3027147074</li>
+							<li>Barrio Ebenezer-Fusagasuga<br> Cundimarca - Colombia</li>
+						</ul>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Redes Sociales
+						</h3>
+						<ul class="redes">
+							<li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="barra-footer">
+				&copy; motivo estudiantil- 2025
+			</div>
+		</footer>
+		<script src="js/jquery.js"></script>
+		<script src="js/funciones.js"></script>
+	</body>
+</html>
